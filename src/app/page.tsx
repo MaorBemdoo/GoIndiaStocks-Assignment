ddcb468a65f82412e6fa36269db7f1510b4e424b
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-03-19 12:57:38 +0100

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import DiscussionForum from "@/components/DiscussionForum";
-import MarketStories from "@/components/MarketStories";
 import ResponsiveTab from "@/components/ResponsiveTab";
 import Sidebar from "@/components/Sidebar";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <Sidebar />
-      {/* <DiscussionForum />
-      <MarketStories /> */}
       <ResponsiveTab />
     </main>
   );
